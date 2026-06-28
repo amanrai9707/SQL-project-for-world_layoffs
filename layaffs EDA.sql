97a36a8dfe5bd69eeb0f2fd4e7a5a1e1738bba2f
Aman Zee <reneoaman666@gmail.com>
2026-06-28 11:01:24 +0530

    Update layaffs EDA.sql

--- layaffs EDA.sql
+++ layaffs EDA.sql
@@ -1,10 +1,3 @@
--- EDA
-
--- Here we are jsut going to explore the data and find trends or patterns or anything interesting like outliers
-
--- normally when you start the EDA process you have some idea of what you're looking for
-
--- with this info we are just going to look around and see what we find!
 
 SELECT * 
 FROM world_layoffs.layoffs_staging2;
@@ -52,9 +45,6 @@ ORDER BY funds_raised_millions DESC;
 
 
 
--- SOMEWHAT TOUGHER AND MOSTLY USING GROUP BY--------------------------------------------------------------------------------------------------
-
--- Companies with the biggest single Layoff
 
 SELECT company, total_laid_off
 FROM world_layoffs.layoffs_staging
@@ -107,7 +97,7 @@ ORDER BY 2 DESC;
 
 
 
--- TOUGHER QUERIES------------------------------------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------
 
 -- Earlier we looked at Companies with the most Layoffs. Now let's look at that per year. It's a little more difficult.
 -- I want to look at 
